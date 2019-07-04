174678d0fdfbca59c8bd1651eacddbe4b37e60b7
Teri Eyenike <teyenike1@gmail.com>
2019-07-04 22:46:10 +0100

    Newsletter API key hidden

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+const api_key = process.env.API_KEY;
+const id_key = process.env.ID_KEY;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/signup.html')
-})
+  res.sendFile(__dirname + '/signup.html');
+});
 
 app.post('/', (req, res) => {
-  let firstname = req.body.fname
-  let lastname = req.body.lname
-  let email = req.body.email
+  let firstname = req.body.fname;
+  let lastname = req.body.lname;
+  let email = req.body.email;
 
   let data = {
     members: [
@@ -27,41 +31,36 @@ app.post('/', (req, res) => {
         }
       }
     ]
-  }
+  };
 
-  let jsonData = JSON.stringify(data)
+  let jsonData = JSON.stringify(data);
 
   let options = {
-    url: 'https://us14.api.mailchimp.com/3.0/lists/fe15ba7a95',
+    url: `https://us14.api.mailchimp.com/3.0/lists/${id_key}`,
     method: 'POST',
     headers: {
-      Authorization: 'terieyenike 9d0946114ee817cb2f779e64b6cefe93-us14'
+      Authorization: `terieyenike ${api_key}`
     },
     body: jsonData
-  }
+  };
 
   request(options, (error, response, body) => {
     if (error) {
-      res.sendFile(__dirname + '/failure.html')
+      res.sendFile(__dirname + '/failure.html');
     } else {
       if (response.statusCode === 200) {
-        res.sendFile(__dirname + '/success.html')
+        res.sendFile(__dirname + '/success.html');
       } else {
-        res.sendFile(__dirname + '/failure.html')
+        res.sendFile(__dirname + '/failure.html');
       }
     }
-  })
-})
+  });
+});
 
 app.post('/failure', (req, res) => {
-  res.redirect('/')
-})
+  res.redirect('/');
+});
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log('server is running on port 4000')
-})
-
-// 9d0946114ee817cb2f779e64b6cefe93-us14
-
-// Unique id
-// fe15ba7a95
+  console.log('server is running on port 4000');
+});
